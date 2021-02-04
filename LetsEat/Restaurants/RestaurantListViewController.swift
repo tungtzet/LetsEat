8aec56d3d5529f1f8e91c2b2a7222a2cdbb4640c
Tung Nguyen <t.nguyen@monks.de>
2021-02-04 14:39:26 +0100

    UI Basic 2

---
+++ LetsEat/Restaurants/RestaurantListViewController.swift
@@ -0,0 +1,40 @@
+//
+//  RestaurantListViewController.swift
+//  LetsEat
+//
+//  Created by Tung Nguyen on 04.02.21.
+//
+
+import UIKit
+
+class RestaurantListViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return collectionView.dequeueReusableCell(withReuseIdentifier: "restaurantCell", for: indexPath)
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
