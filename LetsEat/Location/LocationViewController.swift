13e8609f26bc21dc75557cc59598f075275c8ecc
Tung Nguyen <t.nguyen@monks.de>
2021-02-05 09:04:52 +0100

    location done

--- LetsEat/Location/LocationViewController.swift
+++ LetsEat/Location/LocationViewController.swift
@@ -7,23 +7,25 @@
 
 import UIKit
 
-class LocationViewController: UIViewController {
-
+class LocationViewController: UIViewController, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    let manager = LocationDataManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        manager.fetch()
+    }
 
-        // Do any additional setup after loading the view.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        manager.numberOfItems()
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "locationCell", for: indexPath)
+        cell.textLabel?.text = manager.locationItem(at: indexPath)
+        return cell
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
