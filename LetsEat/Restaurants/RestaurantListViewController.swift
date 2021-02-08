5ae5577b7380b89a8b459fa810938a04cf9239d1
Tung Nguyen <tungnt@arena.edu.vn>
2021-02-08 22:30:38 +0100

    chapter 17 done

--- LetsEat/Restaurants/RestaurantListViewController.swift
+++ LetsEat/Restaurants/RestaurantListViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class RestaurantListViewController: UIViewController, UICollectionViewDelegate {
     
+    var manager = RestaurantDataManager()
     var selectedRestaurant:RestaurantItem?
     var selectedCity:LocationItem?
     var selectedType:String?
@@ -21,34 +22,58 @@ class RestaurantListViewController: UIViewController, UICollectionViewDelegate {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        guard let location = selectedCity?.city, let filter = selectedType else { return }
-        let manager = RestaurantDataManager()
-        manager.fetch(by: location, with: filter) { items in
-            if manager.numberOfItems() > 0 {
-                for item in items {
-                    if let itemName = item.name {
-                        print(itemName)
-                    }
-                }
-            } else {
-                print("No data")
-            }
-        }
+        createData()
+        setupTitle()
     }
 }
 
 // MARK: Private Extension
 private extension RestaurantListViewController {
-    // code goes here
+    func createData() {
+        guard let location = selectedCity?.city, let filter = selectedType else { return }
+        manager.fetch(by: location, with: filter) { _ in
+            if manager.numberOfItems() > 0 {
+                collectionView.backgroundView = nil
+            } else {
+                let view = NoDataView(frame: CGRect(x: 0, y: 0, width: collectionView.frame.width, height: collectionView.frame.height))
+                view.set(title: "Restaurants")
+                view.set(desc: "No restaurants found.")
+                collectionView.backgroundView = view
+            }
+            collectionView.reloadData()
+        }
+    }
+    
+    func setupTitle() {
+        navigationController?.setNavigationBarHidden(false, animated: false)
+        if let city = selectedCity?.city, let state = selectedCity?.state {
+            title = "\(city.uppercased()), \(state.uppercased())"
+        }
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
 }
 
 // MARK: UICollectionViewDataSource
 extension RestaurantListViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        1
+        manager.numberOfItems()
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        return collectionView.dequeueReusableCell(withReuseIdentifier: "restaurantCell", for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "restaurantCell", for: indexPath) as! RestaurantCell
+        let item = manager.restaurantItem(at: indexPath)
+        if let name = item.name { cell.lblTitle.text = name}
+        if let cuisine = item.subtitle { cell.lblCuisine.text = cuisine}
+        if let image = item.imageURL {
+            if let url = URL(string: image) {
+                let data = try? Data(contentsOf: url)
+                if let imageData = data {
+                    DispatchQueue.main.async {
+                        cell.imgRestaurant.image = UIImage(data: imageData)
+                    }
+                }
+            }
+        }
+        return cell
     }
 }
---
+++ LetsEat/Restaurants/View/RestaurantCell.swift
@@ -0,0 +1,15 @@
+//
+//  RestaurantCell.swift
+//  LetsEat
+//
+//  Created by Michael Nguyen on 08.02.21.
+//
+
+import UIKit
+
+class RestaurantCell: UICollectionViewCell {
+    
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var lblCuisine: UILabel!
+    @IBOutlet weak var imgRestaurant: UIImageView!
+}
