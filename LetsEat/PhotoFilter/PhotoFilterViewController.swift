1179741d44477730ccdfb92a08dd2c977fe07734
Tung Nguyen <t.nguyen@monks.de>
2021-02-23 09:10:17 +0100

    extensions of PhotoFilterViewController

--- LetsEat/PhotoFilter/PhotoFilterViewController.swift
+++ LetsEat/PhotoFilter/PhotoFilterViewController.swift
@@ -44,5 +44,69 @@ private extension PhotoFilterViewController {
         checkSource()
     }
     
+    func setupCollectionView(){
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.sectionInset = UIEdgeInsets(top: 7, left: 7, bottom: 7, right: 7)
+        layout.minimumInteritemSpacing = 0
+        layout.minimumLineSpacing = 7
+        collectionView.collectionViewLayout = layout
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
     
+    func checkSource (){
+        let cameraMediaType = AVMediaType.video
+        let cameraAuthorizationStatus = AVCaptureDevice.authorizationStatus(for: cameraMediaType)
+        switch cameraAuthorizationStatus {
+        case .authorized:
+            self.showCameraUserInterface()
+        case .restricted, .denied:
+            break
+        case .notDetermined:
+            AVCaptureDevice.requestAccess(for: cameraMediaType) { granted in
+                if granted {
+                    DispatchQueue.main.async {
+                        self.showCameraUserInterface()
+                    }
+                }
+            }
+        }
+    }
+    
+    func showApplyFilter() {
+        manager.fetch { (items) in
+            filters = items
+            if let image = self.image {
+                imgExample.image = image
+                collectionView.reloadData()
+            }
+        }
+    }
+    @IBAction func onPhotoTapped(_ sender: Any) {
+        checkSource()
+    }
+}
+
+extension PhotoFilterViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        filters.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "filterCell", for: indexPath) as! FilterCell
+        let item = self.filters[indexPath.row]
+        if let img = self.thumbnail {
+            cell.set(image: img, item: item)
+        }
+        return cell
+    }
+}
+
+extension PhotoFilterViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let screenRect = collectionView.frame.size.height
+        let screenHt = screenRect - 14
+        return CGSize(width: 150, height: screenHt)
+    }
 }
